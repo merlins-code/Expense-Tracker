14860b2eaee18ddee8db565f1532aee7c3993cdb
merlins-code <github.6arzv@slmails.com>
2025-07-29 21:10:27 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -6,3 +6,11 @@ create table IF NOT EXISTS Expenses (
     location TEXT NOT NULL,
     category TEXT NOT NULL Check (category in ('Housing', 'Utilities', 'Subscriptions', 'Food', 'Car', 'Miscellaneous', 'Kids'))
 );
+
+create table if not exists Paychecks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    amount REAL NOT NULL,
+    date TEXT NOT NULL,
+    description text,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
